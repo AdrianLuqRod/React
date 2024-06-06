bb884397885c63b6c9309f8e956f616046218b95
Adri <adrian.luque.rod@gmail.com>
2024-06-06 09:12:16 +0200

    tic-tac-toe

---
+++ projects/01-tic-tac-toe/src/App.tsx
@@ -0,0 +1,32 @@
+// import { useState } from "react";
+
+// interface Turns {
+//   X: "x";
+//   O: "o";
+// }
+
+// const TURNS: Turns = {
+//   X: "x",
+//   O: "o",
+// };
+
+// const Square = ({ children, updateBoard, index }) => {
+//   return <div className="square">{children}</div>;
+// };
+
+// function App() {
+//   const [board, setBoard] = useState(Array(9).fill(null));
+//   const [turn, setTurn] = useState(TURNS.X);
+//   return (
+//     <main className="board">
+//       <h1>Tic Tac Toe</h1>
+//       <section className="game">
+//         {board.map((_, index) => {
+//           return <Square key={index} index={index}></Square>;
+//         })}
+//       </section>
+//     </main>
+//   );
+// }
+
+// export default App;
